74ce5bd46276a33ab9e6ad3bd6fafc8895f30243
JoGreen <giorgiomaria@live.it>
2018-03-06 02:16:06 +0100

    setup dependencies installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='community_detection',
+      version='1.1',
+      install_requires=[
+            'pymongo',
+            'mysqlclient'
+      ]
+      )
