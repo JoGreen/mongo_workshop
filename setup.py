fd548087a74e8c7dcbb5eb32a12ca5890b762a9a
JoGreen <giorgiomaria@live.it>
2018-03-06 13:07:22 +0100

    dep

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='community_detection',
       version='1.1',
       install_requires=[
             'pymongo',
-            'mysqlclient'
+            'mysqlclient',
+            'tqdm'
       ]
       )
