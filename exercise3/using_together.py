5324ea664a37d4adb4af5b3a9a0d2c79ad1230d3
JoGreen <giorgiomaria@live.it>
2018-03-11 22:43:39 +0100

    indentation improved

--- exercise3/using_together.py
+++ exercise3/using_together.py
@@ -1,4 +1,8 @@
-####write a method that takes from mysql:
-#distinct paper_id published in 2009
-# from mongo
-#find all paper retrieved in mysql with more than 5 citations
+####write a method that retrieve
+
+    #  from mysql:
+    #distinct paper_id published in 2009
+
+    #  from mongo
+    #find all paper with more than 5 citations
+    # using paper_ids retrieved in mysql
